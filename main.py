a0c957568a59dc68b568d38ef44cf002c5e516eb
MaysDaysYes <ivgen2006@mail.ru>
2026-05-15 12:17:50 +0300

    fix flake8

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def greet():
     return "Hello from peresdacha!"
 
+
 if __name__ == "__main__":
     print(greet())
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,5 +5,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from main import greet
 
+
 def test_greet():
     assert greet() == "Hello from peresdacha!"
